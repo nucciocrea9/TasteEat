29c712c34780fb4a824c03a7812cda9c9605b3d4
nuccio <nuccio.nc96@gmail.com>
2022-05-18 11:16:20 +0200

    test cd

--- src/app/services/cognito.service.ts
+++ src/app/services/cognito.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CognitoAuth } from 'amazon-cognito-auth-js';
 import * as AWS from 'aws-sdk';
-import { environment } from './../../environments/environment';
+import { environment } from './../../environments/environment.prod';
 
 
 @Injectable({
