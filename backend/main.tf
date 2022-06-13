4e1ef18aa41cf1d57c3e935e78988ea118d9d0ae
nuccio <nuccio.nc96@gmail.com>
2022-06-14 01:07:15 +0200

    test

--- backend/main.tf
+++ backend/main.tf
@@ -26,7 +26,7 @@ module "website" {
 
 
 module "authorization-west" {
-  source               = "./authorization"
+  source               = "./modules/authorization"
   authenticated_role   = module.iam.authenticated_role
   unauthenticated_role = module.iam.unauthenticated_role
   region               = var.region
@@ -38,7 +38,7 @@ module "authorization-west" {
 }
 
 module "iam" {
-  source                = "./iam"
+  source                = "./modules/iam"
   //identity_pool_id      = module.authorization.identity_pool
   identity_pool_id_west = module.authorization-west.identity_pool
 }
@@ -52,7 +52,7 @@ module "storage" {
 }
 
 module "storage_west" {
-  source      = "./storage"
+  source      = "./modules/storage"
   cognitoRole = module.iam.authenticated_role
   providers = {
     aws = "aws.us-west-1"
@@ -60,7 +60,7 @@ module "storage_west" {
 
 }
 module "api" {
-  source        = "./API"
+  source        = "./modules/API"
   user_pool_arn = module.authorization-west.user_pool_arn
   db_table      = module.database.table_name
   db_table_order=module.database.table_name_order
@@ -71,7 +71,7 @@ module "api" {
 }
 
 module "api-west" {
-  source        = "./API"
+  source        = "./modules/API"
   user_pool_arn = module.authorization-west.user_pool_arn
   db_table      = module.database-west.table_name
   db_table_order=module.database-west.table_name_order
@@ -82,13 +82,13 @@ module "api-west" {
 }
 
 module "database" {
-  source = "./database"
+  source = "./modules/database"
   providers = {
     aws = "aws"
   }
 }
 module "database-west" {
-  source = "./database"
+  source = "./modules/database"
   providers = {
     aws = "aws.us-west-1"
   }
