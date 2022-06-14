33e8dd500daf4da52986c689cb9190927f81d900
nuccio <nuccio.nc96@gmail.com>
2022-06-14 15:28:14 +0200

    test terraform

--- backend/main.tf
+++ backend/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket12"
+    key = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
